60b12e1dd6c6cb11c5ed275e17316cb4dacb522e
İbrahim Eren Coşkun <ibrahimerencskn4@gmail.com>
2025-03-12 17:02:17 +0300

    add prisma nextauth

---
+++ middleware.ts
@@ -0,0 +1,6 @@
+import { NextRequest } from "next/server";
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+
+const { auth } = NextAuth(authConfig);
+export default auth(async function middleware(req: NextRequest) {});
